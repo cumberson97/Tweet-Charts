adaf716f103e98d837cf7d2bcbe794d03af23079
cumberson97 <60389903+cumberson97@users.noreply.github.com>
2022-11-15 08:34:47 -0400

    changes to the component and logic

--- src/App.js
+++ src/App.js
@@ -1,14 +1,91 @@
 import React from 'react';
 import Chart from './chart';
+import data1 from './twitter_api_response.json'
+import data2 from './facebook_sentiment.json'
 import './App.css';
+
+
+const twitter_timeline =data1.stats.twitter.timelineStats.timeline;
+const facebook_timeline = data2.stats.facebook.timelineStats.timeline;
+
+var display=[],api_res=[data1,data2];
+
+function dateConvert(dates){
+  var val = new Date(dates);
+  var test = val.getFullYear
+  console.log (test)
+  
+  return `${val.getDay}/${val.getMonth}/${val.getFullYear}`
+}
+
+for(const data of api_res){
+  if ("twitter" in data.stats){
+    const twitter_timeline =data.stats.twitter.timelineStats.timeline;
+    var positive= [],negative=[],neutral=[],date=[];
+    for(const timel of twitter_timeline){
+        positive.push(timel.sentimentAsCategories.positiveTweets)
+        negative.push(timel.sentimentAsCategories.negativeTweets)
+        neutral.push(timel.sentimentAsCategories.neutralTweets)
+        date.push(timel.startDate)
+    }
+
+    display.push ({twitter:[ { type:"scatter",
+                    x: date,
+                    y: positive
+                  },{
+                    type:"scatter",
+                    x: date,
+                    y: negative,
+                    xaxis: 'x2',
+                    yaxis: 'y2'
+                  },{
+                    type:"scatter",
+                    x: date,
+                    y: neutral,
+                    xaxis: 'x3',
+                    yaxis: 'y3'
+                  }]});
+
+  }else{
+    const facebook_timeline =data2.stats.facebook.timelineStats.timeline;
+    positive= []; negative=[]; neutral=[]; date=[]; 
+    for(const timel of facebook_timeline){
+        positive.push(timel.sentimentAsCategories.positiveComments)
+        negative.push(timel.sentimentAsCategories.negativeComments)
+        neutral.push(timel.sentimentAsCategories.neutralComments)
+        date.push(timel.startDate)
+    }
+    display.push ({facebook:[ { type:"scatter",
+                    x: date,
+                    y: positive
+                  },{
+                    type:"scatter",
+                    x: date,
+                    y: negative,
+                    xaxis: 'x2',
+                    yaxis: 'y2'
+                  },{
+                    type:"scatter",
+                    x: date,
+                    y: neutral,
+                    xaxis: 'x3',
+                    yaxis: 'y3'
+                  }]});
+  }
+}
+
+
+
 
 function App() {
   return ( 
     <div className="App">
       <h1>Tweet Sentiment Charts</h1>
-      <Chart/>
+      <Chart values = {display[0].twitter}/>
+      <Chart values = {display[1].facebook}/>
     </div>
   );
 }
+
 
 export default App;
--- src/chart.js
+++ src/chart.js
@@ -3,34 +3,10 @@ import Plot from 'react-plotly.js';
 import data from './twitter_api_response.json'
 
 
-function chart(){
-        console.log(barX)
+function chart(props){
         return(<div>
             <Plot
-                data={[
-                    {  
-                        type:"scatter",
-                        x: [date1,date2],
-                        y: [pos1,pos2]
-                    },
-                    {
-                        type:"scatter",
-                        x: [date1,date2],
-                        y: [neg1,neg2],
-                        xaxis: 'x2',
-                        yaxis: 'y2'
-                    },
-                    {
-                        type:"scatter",
-                        x: [date1,date2],
-                        y: [neu1,neu2],
-                        xaxis: 'x3',
-                        yaxis: 'y3'
-                    }
-            
-
-
-                ]}
+                data={props.values}
                 layout ={{grid: {rows: 2, columns: 2, pattern: 'independent',ygap:.35},height:1050,width:1100,annotations:[{
                     text: "Positive",
                       font: {
@@ -77,43 +53,8 @@ function chart(){
 }
 
 
-var test =data.stats.twitter.timelineStats.timeline[0].top20TweetsByFollowers;
-
-var pos1 =data.stats.twitter.timelineStats.timeline[0].sentimentAsCategories.positiveTweets;
-var pos2 =data.stats.twitter.timelineStats.timeline[1].sentimentAsCategories.positiveTweets;
-
-var neg1 =data.stats.twitter.timelineStats.timeline[0].sentimentAsCategories.negativeTweets;
-var neg2 =data.stats.twitter.timelineStats.timeline[1].sentimentAsCategories.negativeTweets;
-
-var neu1 =data.stats.twitter.timelineStats.timeline[0].sentimentAsCategories.neutralTweets;
-var neu2 =data.stats.twitter.timelineStats.timeline[1].sentimentAsCategories.neutralTweets;
-
-var date1 =data.stats.twitter.timelineStats.timeline[0].startDate
-var date2 =data.stats.twitter.timelineStats.timeline[1].startDate
-var barX = [], barY = [];
-var menY = [];
-var sentiY = [],sentimentsN = ['NEGATIVE','POSITIVE','NEUTRAL'], sentimentCount = [0,0,0];
-
-for (const val of test){    
-    barY.push(val.followers)
-    barX.push(val.handler_name)
-    sentiY.push(val.sentimentSubjectivity)
-    
-    if(val.mentions !==null){
-        menY.push(val.mentions.length)
-    }else{
-        menY.push(0)
-    }
-
-    if(val.sentimentPolarityLabel===sentimentsN[0]){
-        sentimentCount[0]++;
-    }else if(val.sentimentPolarityLabel===sentimentsN[1]){
-        sentimentCount[1]++;
-    }else{
-        sentimentCount[2]++;
-    }
 
 
-}
+
 
 export default chart;
