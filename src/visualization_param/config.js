486947ef162789b8c874a0f96d3a07a9346d6a60
cumberson97 <60389903+cumberson97@users.noreply.github.com>
2022-12-07 09:07:42 -0400

    config

---
+++ src/visualization_param/config.js
@@ -0,0 +1,6 @@
+export function config() {
+    return {
+        modeBarButtonsToRemove: ['toImage', 'hoverClosestGeo', 'pan2d', 'select2d', 'lasso2d', 'zoomIn2d', "zoomOut2d", 'hoverClosestPie', 'toggleHover', 'toggleSpikelines', 'autoScale2d', 'resetScale2d', 'zoom2d'],
+        displaylogo: false
+    }
+}
